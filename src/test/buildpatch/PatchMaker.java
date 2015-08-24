2c8270479b1a78dec49353e6836c33ffc507b451
firefoxmmx2 <firefoxmmx@gmail.com>
2015-08-24 14:41:19 +0800

    update patchmaker ,support multiple configs

--- src/test/buildpatch/PatchMaker.java
+++ src/test/buildpatch/PatchMaker.java
@@ -6,58 +6,108 @@ import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 一个简单的临时的补丁打包工具
  */
 public class PatchMaker {
+    private Map<String,Map<String,String>> configMap;
+    private Map<String,String> config;
+
     private boolean isDebug = true;
 
-    private String PROJECT_ROOT = "/home/hooxin/Work/加油站散装油系统";
+    private String PROJECT_ROOT;
     /**
      * 部署在服务器上面的地址
      */
-    private String PROJECT_ARTIFACT_DIR = PROJECT_ROOT + "/out/artifacts/_war_exploded";
+    private String PROJECT_ARTIFACT_DIR;
     /**
      * class文件夹名称
      */
-    private String CLASSES_DIR = "/WEB-INF/classes";
+    private String CLASSES_DIR ;
     /**
      * class文件夹的真实路径
      */
-    private String PROJECT_ARTIFACT_CLASSES_DIR = PROJECT_ARTIFACT_DIR + CLASSES_DIR;
+    private String PROJECT_ARTIFACT_CLASSES_DIR;
     /**
      * 补丁输出文件夹
      */
-    private String PATCH_OUT_DIR = "/home/hooxin/Work/加油站散装油系统/补丁/内保单位综合系统v2.3.3-v2.3.5";
+    private String PATCH_OUT_DIR;
     /**
      * 改动补丁文件夹列表(可以直接使用idea格式)
      */
-    private String INPUT_FILE = "/home/hooxin/加油站散装油文件更新列表.txt";
+    private String INPUT_FILE;
     /**
      * 源码目录
      */
-    private String SOURCE_DIR = "源码";
+    private String SOURCE_DIR;
     /**
      * java源码目录
      */
-    private String JAVA_SOURCE_DIR = "src";
+    private String JAVA_SOURCE_DIR;
     /**
      * 数据库脚本目录
      */
-    private String DATABASE_SCRIPT_DIR = "数据库脚本";
+    private String DATABASE_SCRIPT_DIR;
     /**
      * 网页根目录
      */
-    private String WEB_ROOT = "WebRoot";
+    private String WEB_ROOT;
 
     private List<String> EXCLUDE_WORDS=new ArrayList<String>();
 
+    public PatchMaker() {
+        configMap=new HashMap<>();
+        //广西内保
+        Map<String,String> config=new HashMap<>();
+        config.put("PROJECT_ROOT","/home/hooxin/Work/加油站散装油系统");
+        config.put("PROJECT_ARTIFACT_DIR",config.get("PROJECT_ROOT")+"/out/artifacts/_war_exploded");
+        config.put("CLASSES_DIR","/WEB-INF/classes");
+        config.put("PROJECT_ARTIFACT_CLASSES_DIR",config.get("PROJECT_ARTIFACT_DIR")+config.get("CLASSES_DIR"));
+        config.put("PATCH_OUT_DIR","/home/hooxin/Work/加油站散装油系统/补丁/内保单位综合系统v2.4.2-v2.4.3");
+        config.put("INPUT_FILE","/home/hooxin/加油站散装油文件更新列表.txt");
+        config.put("SOURCE_DIR","源码");
+        config.put("JAVA_SOURCE_DIR","src");
+        config.put("DATABASE_SCRIPT_DIR","数据库脚本");
+        config.put("WEB_ROOT","WebRoot");
+        configMap.put("gxnbdw",config);
+
+
+        //海南社采
+        config=new HashMap<>();
+        config.put("PROJECT_ROOT","/home/hooxin/Work/海南社采平台") ;
+        config.put("PROJECT_ARTIFACT_DIR",config.get("PROJECT_ROOT")+"/out/artifacts/hnscpt_war_exploded");
+        config.put("CLASSES_DIR","/WEB-INF/classes");
+        config.put("PROJECT_ARTIFACT_CLASSES_DIR",config.get("PROJECT_ARTIFACT_DIR")+config.get("CLASSES_DIR"));
+        config.put("PATCH_OUT_DIR","/home/hooxin/Work/海南社采平台/补丁/海南社采平台v1.0-v1.1补丁");
+        config.put("INPUT_FILE","/home/hooxin/海南社采更新列表.txt");
+        config.put("SOURCE_DIR","hnscpt");
+        config.put("JAVA_SOURCE_DIR","src");
+        config.put("DATABASE_SCRIPT_DIR","数据库脚本");
+        config.put("WEB_ROOT","WebRoot");
+        configMap.put("hnscpt",config);
+    }
+
     //初始化
-    public PatchMaker(){
+    public PatchMaker(String configName){
+        this();
+        config = configMap.get(configName);
         //默认添加补丁为排除关键字
         EXCLUDE_WORDS.add("补丁");
+
+        PROJECT_ROOT=config.get("PROJECT_ROOT");
+        PROJECT_ARTIFACT_DIR=config.get("PROJECT_ARTIFACT_DIR");
+        PROJECT_ARTIFACT_CLASSES_DIR=config.get("PROJECT_ARTIFACT_CLASSES_DIR");
+        PATCH_OUT_DIR= config.get("PATCH_OUT_DIR");
+        INPUT_FILE= config.get("INPUT_FILE");
+        SOURCE_DIR = config.get("SOURCE_DIR");
+        JAVA_SOURCE_DIR=config.get("JAVA_SOURCE_DIR");
+        DATABASE_SCRIPT_DIR= config.get("DATABASE_SCRIPT_DIR");
+        CLASSES_DIR = config.get("CLASSES_DIR");
+        WEB_ROOT= config.get("WEB_ROOT");
     }
     /**
      * 打包补丁
@@ -152,7 +202,7 @@ public class PatchMaker {
     }
 
     public static void main(String[] args) throws IOException {
-        PatchMaker pm = new PatchMaker();
+        PatchMaker pm = new PatchMaker("hnscpt");
         pm.buildPatch();
     }
 }
